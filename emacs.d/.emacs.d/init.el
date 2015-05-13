a0bfa27525c197aa88c0a57529dee42a75f82c29
rastian <bronxchris96@gmail.com>
2015-05-13 13:34:56 -0400

    Updated files

---
+++ emacs.d/.emacs.d/init.el
@@ -0,0 +1,43 @@
+(org-babel-load-file "~/.emacs.d/christian.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+	 ["#151718" "#CE4045" "#9FCA56" "#DCCD69" "#55B5DB" "#A074C4" "#55B5DB" "#D4D7D6"])
+ '(ansi-term-color-vector
+	 [unspecified "#151718" "#CE4045" "#9FCA56" "#DCCD69" "#55B5DB" "#A074C4" "#55B5DB" "#D4D7D6"])
+ '(cursor-type (quote bar))
+ '(custom-safe-themes
+	 (quote
+		("94ba29363bfb7e06105f68d72b268f85981f7fba2ddef89331660033101eb5e5" "e80932ca56b0f109f8545576531d3fc79487ca35a9a9693b62bf30d6d08c9aaf" default)))
+ '(fci-rule-color "#383838")
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+	 (quote
+		((20 . "#BC8383")
+		 (40 . "#CC9393")
+		 (60 . "#DFAF8F")
+		 (80 . "#D0BF8F")
+		 (100 . "#E0CF9F")
+		 (120 . "#F0DFAF")
+		 (140 . "#5F7F5F")
+		 (160 . "#7F9F7F")
+		 (180 . "#8FB28F")
+		 (200 . "#9FC59F")
+		 (220 . "#AFD8AF")
+		 (240 . "#BFEBBF")
+		 (260 . "#93E0E3")
+		 (280 . "#6CA0A3")
+		 (300 . "#7CB8BB")
+		 (320 . "#8CD0D3")
+		 (340 . "#94BFF3")
+		 (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
