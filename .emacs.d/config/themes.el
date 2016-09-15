9902a9366722f26b74bce6107e64f277c22e2392
Christian LaVerde <bronxchris96@gmail.com>
2016-09-15 17:23:26 +0100

    Update themes

--- .emacs.d/config/themes.el
+++ .emacs.d/config/themes.el
@@ -1,37 +1,42 @@
-(when window-system
-  (use-package spacemacs-theme
-    :disabled t
-    :ensure t
-    :config
-    ;; (load-theme 'spacemacs-light t)
-    (load-theme 'spacemacs-dark t))
+(use-package spacemacs-theme
+  :disabled t
+  :ensure t
+  :config
+  ;; (load-theme 'spacemacs-light t)
+  (load-theme 'spacemacs-dark t))
 
-  (use-package solarized-theme
-    :disabled t
-    :ensure t
-    :config
-    (load-theme 'solarized-dark t))
+(use-package solarized-theme
+  :disabled t
+  :ensure t
+  :config
+  (load-theme 'solarized-dark t))
 
-  (use-package zenburn-theme
-    :disabled t
-    :ensure t
-    :config
-    (load-theme 'zenburn t))
+(use-package zenburn-theme
+  :disabled t
+  :ensure t
+  :config
+  (load-theme 'zenburn t))
 
-  (use-package seti-theme
-    :disabled t
-    :ensure t
-    :config
-    (load-theme 'seti t))
+(use-package seti-theme
+  :disabled t
+  :ensure t
+  :config
+  (load-theme 'seti t))
 
-  (use-package material-theme
-    :disabled t
-    :ensure t
-    :config
-    (load-theme 'material t))
+(use-package material-theme
+  :disabled t
+  :ensure t
+  :config
+  (load-theme 'material t))
 
-  (use-package monokai-theme
-    :disabled t
-    :ensure t
-    :config
-    (load-theme 'monokai t)))
+(use-package monokai-theme
+  :disabled t
+  :ensure t
+  :config
+  (load-theme 'monokai t))
+
+(use-package darktooth-theme
+  ;; :disabled t
+  :ensure t
+  :config
+  (load-theme 'darktooth t))
