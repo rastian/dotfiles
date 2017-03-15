c4bb06d21a887294a9963f127d6f00c15f1bc4fb
Christian LaVerde <bronxchris96@gmail.com>
2017-03-15 04:42:13 +0100

    Removed heading font size in org files

--- .emacs.d/config/package-setup.el
+++ .emacs.d/config/package-setup.el
@@ -48,27 +48,7 @@
    'org-babel-load-languages
    '((python . t)
      (java . t)))
-  (org-toggle-pretty-entities)
-  ;; testing. changing header size and font based on level
-  (when window-system 
-    (let* ((variable-tuple (cond ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
-				 ((x-list-fonts "Lucida Grande")   '(:font "Lucida Grande"))
-				 ((x-list-fonts "Verdana")         '(:font "Verdana"))
-				 ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
-				 (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
-	   (base-font-color     (face-foreground 'default nil 'default))
-	   (headline           '(:inherit default :weight bold :foreground ,base-font-color)))
-
-      (custom-theme-set-faces 'user
-			      '(org-level-8 ((t (,@headline ,@variable-tuple))))
-			      '(org-level-7 ((t (,@headline ,@variable-tuple))))
-			      '(org-level-6 ((t (,@headline ,@variable-tuple))))
-			      '(org-level-5 ((t (,@headline ,@variable-tuple))))
-			      '(org-level-4 ((t (,@headline ,@variable-tuple :height 1))))
-			      '(org-level-3 ((t (,@headline ,@variable-tuple :height 1.1))))
-			      '(org-level-2 ((t (,@headline ,@variable-tuple :height 1.25))))
-			      '(org-level-1 ((t (,@headline ,@variable-tuple :height 1.5))))
-			      '(org-document-title ((t (,@headline ,@variable-tuple :height 1.5 :underline nil))))))))
+  (org-toggle-pretty-entities))
 
 (use-package org-bullets
   :ensure t
